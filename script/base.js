ad7f720c2edda1a2722bc932f9ef50f0b2ed6ab3
Adrian Hawkes <hawkes.adrian@gmail.com>
2020-04-16 12:51:19 +0100

    clean copy of basic template

--- script/base.js
+++ script/base.js
@@ -10,27 +10,9 @@ request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
-// if you want to use a function onload
+// if you want to use a function onload - i have included a test to see what format the json request has
 
 request.onload = function() {
-//   console.log(request.response);
-//   for(let i=0; i<request.response.prizes.length; i++){
-//     for(let j=0; j<request.response.prizes[i].laureates.length; j++){
-//   $("#info").append(`<h4> ${request.response.prizes[i].year} </h4><p> ${request.response.prizes[i].category} - ${request.response.prizes[i].laureates[j].firstname} ${request.response.prizes[i].laureates[j].surname}: ${request.response.prizes[i].laureates[j].motivation}</p>`);
-// }
-// }
+  console.log(request.response)
 }
-
-// example of function which allows you to extract data and send it to the <p id="info">
-
-function fullList(){
-  $("#info").html("");
-  for(let i=0; i<request.response.prizes.length; i++){
-    for(let j=0; j<request.response.prizes[i].laureates.length; j++){
-  $("#info").append(`<h4> ${request.response.prizes[i].year} </h4><p> ${request.response.prizes[i].category} - ${request.response.prizes[i].laureates[j].firstname} ${request.response.prizes[i].laureates[j].surname}: ${request.response.prizes[i].laureates[j].motivation}</p>`);
-}
-}
-}
-
-
 
